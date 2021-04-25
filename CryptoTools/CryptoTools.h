54df0ac2a3d52a08326d55cbb5b38a37c7d9f364
Chris Downie <cdownie@gmail.com>
2021-04-25 13:34:25 -0600

    Create CrytpoTools framework

---
+++ CryptoTools/CryptoTools.h
@@ -0,0 +1,18 @@
+//
+//  CryptoTools.h
+//  CryptoTools
+//
+//  Created by Chris Downie on 4/25/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CryptoTools.
+FOUNDATION_EXPORT double CryptoToolsVersionNumber;
+
+//! Project version string for CryptoTools.
+FOUNDATION_EXPORT const unsigned char CryptoToolsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CryptoTools/PublicHeader.h>
+
+
