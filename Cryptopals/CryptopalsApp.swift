6f92483ff2f4734888da1707971397315c21dcbd
Chris Downie <cdownie@gmail.com>
2021-04-25 13:31:26 -0600

    Initial Commit

---
+++ Cryptopals/CryptopalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptopalsApp.swift
+//  Cryptopals
+//
+//  Created by Chris Downie on 4/25/21.
+//
+
+import SwiftUI
+
+@main
+struct CryptopalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
